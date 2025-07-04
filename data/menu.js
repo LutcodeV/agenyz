5911eb957d69520f9b7614394bfe060944ad687b
Vyacheslav Lutsenko <vyacheslavlutsenkoml@gmail.com>
2025-07-04 10:44:44 +0500

    feat: new changes

--- data/menu.js
+++ data/menu.js
@@ -1,33 +1,7 @@
 export const MENU = [
-	{ title: 'О компании', href: '/about', hiddenOnMobile: true },
-	{ title: 'Продукция', href: '/products', hiddenOnMobile: true },
-	{ title: 'Производство', href: '/production', hiddenOnMobile: true },
-	{ title: 'Биохакинг', href: '/biohacking', hiddenOnMobile: true },
-	{
-		title: 'Каталог',
-		href: '/',
-		submenu: [
-			{ title: 'О компании', href: '/about' },
-			{ title: 'Продукция', href: '/products' },
-			{ title: 'Производство', href: '/production' },
-			{ title: 'Биохакинг', href: '/biohacking' }
-		],
-	},
-	{
-		title: 'О компании',
-		href: '/about',
-		submenu: [
-			{ title: 'О компании', href: '/about' },
-			{ title: 'Продукция', href: '/products' },
-			{ title: 'Производство', href: '/production' },
-			{ title: 'Биохакинг', href: '/biohacking' }
-		],
-	},
-	{ title: 'Пункты выдачи', href: '/pickup', },
-	{ title: 'Новости', href: '/news', },
-	{ title: 'Политика конфиденциальности', href: '/privacy', },
-	{ title: 'Контакты', href: '/contacts', },
-	{ title: 'Реквизиты', href: '/details', },
-	{ title: 'Условия оплаты и доставки', href: '/shipping', },
-	{ title: 'Оферта', href: '/offer', }
-]
+	{ title: "В магазин", href: "/catalog" },
+	{ title: "О компании", href: "/about" },
+	{ title: "Продукция", href: "/products" },
+	{ title: "Производство", href: "/production" },
+	{ title: "Биохакинг", href: "/biohacking" },
+];
